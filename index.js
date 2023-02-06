445fd60220059a1cadb36e69c4b0f994ab8fc2db
Soulbash <georgeolahjr@gmail.com>
2023-02-06 14:06:26 -0500

    finshed mobile nav

---
+++ index.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const aria = document.querySelector("aria-expanded");
+// When someone clicks the hambuger menu
+navToggle.addEventListener("click", () => {
+	const visiblity = nav.getAttribute("data-visible");
+	// If the nav if closed, open it
+	if (visiblity === "false") {
+		nav.setAttribute("data-visible", true);
+		navToggle.setAttribute("aria-expanded", true);
+		// if the nav is open, close it
+	} else {
+		nav.setAttribute("data-visible", false);
+		navToggle.setAttribute("aria-expanded", false);
+	}
+	console.log(navToggle.getAttribute("aria-expanded"));
+});
